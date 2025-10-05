d6da64d27e0932a3e5f03fc2aae4f0afd1a5a583
jram <jram6269@gmail.com>
2025-10-05 16:07:04 +0530

    New Updated Commit

--- Backend/test.py
+++ Backend/test.py
@@ -1,1 +1,9 @@
-SUPERSAHANA
+import yagmail
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+email = os.getenv("email")
+password = os.getenv("password")
+ya = yagmail.SMTP(email,password)
+send = ya.send(to="jram6269@gmail.com", subject="New Student Joined!", contents="Your New Class Details! \nClass ID: 'MATH-35T49' \nClass-Name: 'MATHS'")
